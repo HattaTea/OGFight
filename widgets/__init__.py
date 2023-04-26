8f4abc1503dc19f71dedc2ea2554325b31ff0491
HattaTea <126908352+HattaTea@users.noreply.github.com>
2023-04-26 16:21:40 +0200

    Delete widgets directory

--- widgets/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding : utf-8
-
-from .FGridLayout import FGridLayout
-from .MyCheckBox import MyCheckBox
-from .MyDropDown import MyDropDown
-from .Label_Button import Label_Button
-from .Fond import Fond
